c82b226723556d350960043c80484323e9b4c1bb
unknown <michaeldean.belen@gmail.com>
2023-10-10 15:29:12 +0800

    commit

--- toolTech.js
+++ toolTech.js
@@ -1,10 +1,27 @@
 const aboutSummary = document.querySelector(".AboutMeSummary");
-
+const date = new Date();
+let dateUpadte = date.getDate();
 const updateAboutSummary = () => {
   const textArea = document.createElement("textarea");
   textArea.classList.add("textArea");
 
-  textArea.innerHTML = "hello";
+  const commentBtn = document.createElement("button");
+  commentBtn.classList.add("clickComment");
+  commentBtn.innerHTML = "Add comment";
+
+  commentBtn.addEventListener("click", (e) => {
+    const createComment = document.createElement("div");
+
+    const closestDiv = commentBtn
+      .closest(".AboutMeSummary")
+      .querySelector(".textArea");
+
+    createComment.innerText = `${closestDiv.value} ${date}`;
+    textArea.value = "";
+    aboutSummary.appendChild(createComment);
+  });
+
   aboutSummary.appendChild(textArea);
+  aboutSummary.appendChild(commentBtn);
 };
 updateAboutSummary();
